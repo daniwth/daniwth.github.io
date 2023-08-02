ddd69b2c6289cf074889af333000c236731bb80b
Daniel <93330784+daniwth@users.noreply.github.com>
2023-08-02 19:45:02 +0200

    c

--- projects.js
+++ projects.js
@@ -1,8 +1,7 @@
-// script.js
+// projects.js
 
-// Replace 'YOUR_GITHUB_USERNAME' with your actual GitHub username
 const username = 'daniwth';
-const reposEndpoint = `https://api.github.com/users/${username}/repos?sort=updated&per_page=3`;
+const reposEndpoint = `https://api.github.com/users/${username}/repos`;
 
 async function fetchRepos() {
   try {
@@ -15,19 +14,41 @@ async function fetchRepos() {
 }
 
 function displayRepos(repos) {
-  const projectsSection = document.getElementById('projects');
-  const projectsHTML = repos.map(repo => createRepoHTML(repo)).join('');
-  projectsSection.innerHTML = projectsHTML;
+  const projectsList = document.getElementById('projectsList');
+  projectsList.innerHTML = '';
+
+  const searchInput = document.getElementById('searchInput');
+  const filterLanguage = document.getElementById('filterLanguage');
+
+  repos.forEach(repo => {
+    if (
+      (!searchInput.value || repo.name.toLowerCase().includes(searchInput.value.toLowerCase())) &&
+      (filterLanguage.value === 'all' || repo.language === filterLanguage.value)
+    ) {
+      projectsList.innerHTML += createRepoHTML(repo);
+    }
+  });
 }
 
 function createRepoHTML(repo) {
   return `
-    <h3>${repo.name}</h3>
-    <p>${repo.description || 'No description available.'}</p>
-    <p>Language: ${repo.language || 'N/A'}</p>
-    <p>URL: <a href="${repo.html_url}" target="_blank">${repo.html_url}</a></p>
-    <hr>
+    <div class="project-item">
+      <h3>${repo.name}</h3>
+      <p>${repo.description || 'No description available.'}</p>
+      <p>Language: ${repo.language || 'N/A'}</p>
+      <button><a href="${repo.html_url}" target="_blank">See Project</a></button>
+      <button><a href="projects/${repo.name.toLowerCase()}.html">More info</a></button>
+      <hr>
+    </div>
   `;
 }
 
-fetchRepos();
+document.addEventListener('DOMContentLoaded', () => {
+  fetchRepos();
+  
+  const searchInput = document.getElementById('searchInput');
+  const filterLanguage = document.getElementById('filterLanguage');
+
+  searchInput.addEventListener('input', fetchRepos);
+  filterLanguage.addEventListener('change', fetchRepos);
+});
