e24eccc9a4411f155bafbd55f71bdedd74c7a803
Daniel <93330784+daniwth@users.noreply.github.com>
2023-08-02 18:00:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Function to fetch recent projects from GitHub API and render them on the page
 function renderRecentProjects() {
-    fetch('https://api.github.com/users/daniwth/repos?sort=created')
+    fetch('https://api.github.com/users/tu-usuario/repos?sort=created')
         .then(response => response.json())
         .then(data => {
-            const projectsContainer = document.getElementById('projects');
-            const projects = data.slice(0, 7); // Display the 5 most recent projects
-            
+            const projectsContainer = document.getElementById('projects-list');
+            const projects = data.slice(0, 3); // Display the 3 most recent projects
+
             projects.forEach(project => {
                 const projectElement = document.createElement('a');
                 projectElement.href = project.html_url;
